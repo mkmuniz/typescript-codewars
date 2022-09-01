f45641525f2d3988eafb5c65cc29381e9c20ae52
worstp <mikaelmuniz2001@gmail.com>
2022-08-31 23:02:13 -0300

    8kyu: LongestStringInArray

---
+++ 7kyu/LongestStringInArray.ts
@@ -0,0 +1,17 @@
+export function longestConsec(strarr: string[], k: number): string {
+    var arrConc: string[] = [];
+    var longestWord: string = "";
+    var n: number = strarr.length;
+    if (n === 0 || k > n || k <= 0) return ""
+
+    for (let i: number = 0; i < n; i++) {
+        let value = strarr.slice(i, k + i).join("");
+        arrConc.push(value)
+    }
+
+    arrConc.forEach((word: string) => {
+        if (word.length > longestWord.length) longestWord = word;
+    })
+
+    return longestWord;
+}
